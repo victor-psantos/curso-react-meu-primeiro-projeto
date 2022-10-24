96352a65caa3e0ff1c54687694232a51af7a7bdd
unknown <victor.procopio94@gmail.com>
2022-10-24 16:15:21 -0300

    commit one

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (    
+      <div>
+        <h1>Lista de Compras</h1>
+        <ul>
+          <li>Pão</li>
+          <li>Banana</li>
+          <li>Milk Shake de Doce de Leite</li>
+          <li>Nutella</li>
+          <li>Sorvete</li>
+        </ul>
+      </div>    
+  );
+}
+
+export default App;
